319239ba53d2493c1f33fac6f00a35776eaa3aaa
Himanshu Hansaria <himanshu.hansaria@gmail.com>
2023-04-28 02:19:16 -0400

    Added change of thread script to setup

--- generative/scripts/pretraining_old.py
+++ generative/scripts/pretraining_old.py
@@ -189,7 +189,8 @@ def get_lr(optimizer):
 
 def setup(rank, world_size):    
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'    
+    os.environ['MASTER_PORT'] = '12355'
+    os.environ['OPENBLAS_NUM_THREADS'] = '1'    
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def cleanup():
